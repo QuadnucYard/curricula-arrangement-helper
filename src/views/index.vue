7630b239ca30d616eb82018825f830783f045f79
QuadnucYard <2380433991@qq.com>
2022-08-24 14:51:36 +0800

    从electron迁移

--- src/views/index.vue
+++ src/views/index.vue
@@ -11,15 +11,12 @@
 </template>
 
 <script setup lang="ts">
-import { ipcRenderer } from "electron";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
 import LeftMenu from "./components/LeftMenu.vue";
 import { ElMessage, ElMessageBox } from "element-plus";
 import TopTabs from "./components/TopTabs.vue";
 import MainMenu from "./components/MainMenu.vue";
-
-const remote = require("@electron/remote");
 
 const router = useRouter();
 router.push({ name: "edit" });
@@ -41,38 +38,38 @@ onMounted(() => {
       .catch(() => {});
   });
 
-  menu.value.on("open-file", async () => {
-    const result = await remote.dialog.showOpenDialog({
-      title: "打开文件",
-      properties: ["openFile"],
-      filters: [{ name: "CAH File", extensions: ["cah"] }],
-    });
-    console.log(result);
-    if (!result.canceled && result.filePaths?.length > 0) {
-      store.commit("openFile", { path: result.filePaths[0] });
-      ElMessage.success("成功打开文件");
-    }
-  });
+//   menu.value.on("open-file", async () => {
+//     const result = await remote.dialog.showOpenDialog({
+//       title: "打开文件",
+//       properties: ["openFile"],
+//       filters: [{ name: "CAH File", extensions: ["cah"] }],
+//     });
+//     console.log(result);
+//     if (!result.canceled && result.filePaths?.length > 0) {
+//       store.commit("openFile", { path: result.filePaths[0] });
+//       ElMessage.success("成功打开文件");
+//     }
+//   });
 
-  menu.value.on("save-file", async () => {
-    const activeFile = store.state.activeFile;
-    if (activeFile.path) {
-      activeFile.save();
-      ElMessage.success("成功保存文件");
-    } else {
-      const result = await remote.dialog.showSaveDialog({
-        title: "保存",
-        properties: ["openFile"],
-        filters: [{ name: "CAH File", extensions: ["cah"] }],
-        defaultPath: activeFile.curricula.name,
-      });
-      if (!result.canceled) {
-        activeFile.save(result.filePath);
-        ElMessage.success("成功保存文件");
-      }
-      console.log(result);
-    }
-  });
+//   menu.value.on("save-file", async () => {
+//     const activeFile = store.state.activeFile;
+//     if (activeFile.path) {
+//       activeFile.save();
+//       ElMessage.success("成功保存文件");
+//     } else {
+//       const result = await remote.dialog.showSaveDialog({
+//         title: "保存",
+//         properties: ["openFile"],
+//         filters: [{ name: "CAH File", extensions: ["cah"] }],
+//         defaultPath: activeFile.curricula.name,
+//       });
+//       if (!result.canceled) {
+//         activeFile.save(result.filePath);
+//         ElMessage.success("成功保存文件");
+//       }
+//       console.log(result);
+//     }
+//   });
 });
 </script>
 
